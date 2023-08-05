85560eee5f0aa91dd07929bff80f09aab140beab
Doniyor Doniyorov <ddonierov96@gmail.com>
2023-08-05 12:11:57 +0500

    all

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const global = 'https://crm-backend-5jb6.onrender.com'
 const local = 'http://127.0.0.1:8000'
-axios.defaults.baseURL =  local
+axios.defaults.baseURL =  global
 
 
 export default axios
